bf0f9c778d3f6a4c68217e32fec847e110e24d55
Luke Tchang <luketchang@Alexs-MacBook-Air.local>
2020-06-13 21:32:47 -0700

    fixed syntax error

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -48674,7 +48674,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49744" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50685" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
